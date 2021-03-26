2eb1b6faf4ab184084c4ffb372117bf85ea8989b
tusshar77 <61848185+tusshar77@users.noreply.github.com>
2021-03-27 03:49:17 +0530

    27/03/2021-- 03:49 AM

---
+++ Hanumaddham/events.php
@@ -0,0 +1,88 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hanumaddham </title>
+    <link href="style2.css" rel="stylesheet">
+    <style>
+    </style>
+  </head>
+  <body>
+    <div class="header">
+      <div class="head-1">
+
+        <div class="head-1-1">
+          <img src="img/logo.png" alt="logo" style= "height:100%; width:100%">
+        </div>
+        <div class="head-1-2">
+          <a href="index.php">Hanumaddham</a>
+        </div>
+
+      </div>
+
+      <div class="head-2">
+
+        <div class="head-2-1" >
+          <a href="homepage.php"> Home</a>
+        </div>
+
+        <div class="head-2-2" >
+          <a href="gallery.php">  Gallery</a>
+        </div>
+
+        <div class="head-2-3" style="background-color: #01B5F9 !important; border-top-right-radius: 10px; border-top-left-radius: 10px;">
+          <a href="events.php">  Events</a>
+        </div>
+
+        <div class="head-2-4">
+          <a href="about.php">  About</a>
+        </div>
+
+        <div class="head-2-5">
+          <a href="contact.php">  Contact</a>
+        </div>
+
+      </div>
+    </div>
+
+    
+    <div class="footer">
+      <div class="card-heading-box">
+        <span class="card-heading" style="color: white;">
+            hamara pata
+        </span>
+      </div>
+
+      <div class="footer-card">
+      <div class="card-content-box" style="height:auto;">
+        <span class="card-content" style="color: white; font-size:15px;"> हनुमद धाम आपकी आवश्यकताओं को पूरा करने के लिए प्रतिबद्ध है। प्रश्न, टिप्पणी या विशेष अनुरोध? हम आपसे सुनना पसंद करते हैं, इसलिए हम तक पहुंचने में संकोच न करें।
+            <br><br>पता :-   हनुमद्धाम रोड, विकासनगर देहरादून (उत्तराखंड)
+            <br>पिन कोड  :- 248198
+            <br>hanumad.dham.vn@gmail.com
+            <br>6395206598 ( विष्णु महावर )
+            <br>
+            <br><b>www.hanumaddham.org</b>
+        </span>
+      </div>
+      <hr></hr>
+      <div class="social-media">
+        <div class="social-media-link-box">
+              <span class="logo"><a href="https://www.facebook.com"><img src="img/fb.png"></a></span>
+        </div>
+        <div class="social-media-link-box">
+          <span class="logo"><a href="https://www.facebook.com"><img src="img/youtube.png"></a></span>
+        </div>
+        <div class="social-media-link-box">
+          <span class="logo"><a href="https://www.facebook.com"><img src="img/linkedin.png"></a></span>
+        </div>
+        <div class="social-media-link-box">
+          <span class="logo"><a href="https://www.facebook.com"><img src="img/instagram.png"></a></span>
+        </div>
+      </div>
+    </div>
+
+    </div>
+
+
+  </body>
+</html>
---
+++ Hanumaddham/homepage.php
@@ -0,0 +1,201 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hanumaddham </title>
+    <link href="style2.css" rel="stylesheet">
+    <style>
+    </style>
+  </head>
+  <body>
+    <div class="header">
+      <div class="head-1">
+
+        <div class="head-1-1">
+          <img src="img/logo.png" alt="logo" style= "height:100%; width:100%">
+        </div>
+        <div class="head-1-2">
+          <a href="index.php">Hanumaddham</a>
+        </div>
+
+      </div>
+
+      <div class="head-2">
+
+        <div class="head-2-1" style="background-color: #01B5F9 !important; border-top-right-radius: 10px; border-top-left-radius: 10px;">
+          <a href="homepage.php"> Home</a>
+        </div>
+
+        <div class="head-2-2">
+          <a href="gallery.php">  Gallery</a>
+        </div>
+
+        <div class="head-2-3">
+          <a href="events.php">  Events</a>
+        </div>
+
+        <div class="head-2-4">
+          <a href="about.php">  About</a>
+        </div>
+
+        <div class="head-2-5">
+          <a href="contact.php">  Contact</a>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- Wrapper-->
+    <div class="middle1">
+      <div class="middle1-image1">
+        <!-- Main Image class -->
+      </div>
+    </div>
+
+    <div class="middle2">
+      <div class="middle2-1">
+          <div class=middle2-1-image>
+          </div>
+      </div>
+      <div class="middle2-2">
+
+          <div class="middle2-2-1">
+              <span class="middle2-2-1-text">xg'db\ wfd s] af/] d]<span>
+          </div>
+
+          <div class="middle2-2-2">
+            <span class="middle2-2-2-text">
+              प्रिय श्रद्धालुजन <br>
+                <br>
+              पवित्र सलिला यमुना के तट बसा, उत्तर प्रदेश के पश्चिमी सीमांत पर स्थित तथा हिमचाल प्रदेश की सिमा से लगता हुआ विकासनगर
+              निरंतर विकास की और अग्रसर है । हिमालय की गोद में सुरक्षित यह नगर जनसँख्या की दृष्टि से लगातार बाद रहा है तथा उसी के
+              अनुरूप अपने आकार विस्तार में भी सतत प्रगति कर रहा है। इस नगर के चरों और लगता हुआ विश्तृत ग्रामीण श्रेय है तथा
+              इस नगर - बाबर के पर्वतीय क्षेत्र का यही एक........
+            <span>
+          </div>
+          <div class="middle2-2-3">
+            <span class="middle2-2-3-text">  Read More<span>
+          </div>
+
+      </div>
+    </div>
+
+    <div class="middle3">
+      <div class="card">
+          <div class="card-image-box">
+            <div class="card-image" style="background-image: url(img/chikitsha.jpg);">
+            </div>
+          </div>
+
+          <div class="card-heading-box">
+            <span class="card-heading">Chikitsha</span>
+          </div>
+          <div class="card-content-box">
+            <span class="card-content">पवित्र सलिला यमुना के तट बसा, उत्तर प्रदेश के पश्चिमी सीमांत पर स्थित तथा हिमचाल प्रदेश की सिमा से लगता हुआ विकासनगर निरंतर विकास
+                की और अग्रसर है । हिमालय की गोद में सुरक्षित यह नगर जनसँख्या की दृष्टि से लगातार बाद रहा है तथा उसी के अनुरूप अपने आकार विस्तार में भी सतत प्रगति कर रहा है।  इस नगर के चरों
+                और लगता हुआ विश्तृत ग्रामीण श्रेय है तथा यमुना पार जौनसार - बाबर के पर्वतीय क्षेत्र का यही एक….
+            </span>
+          </div>
+          <div class="card-button-box">
+            <span class="card-button">  <a href="chikitsha.php">Read More</a><span>
+          </div>
+      </div>
+
+      <div class="card">
+          <div class="card-image-box">
+            <div class="card-image" style="background-image: url(img/goshala.jpg);">
+            </div>
+          </div>
+
+          <div class="card-heading-box">
+            <span class="card-heading">Goshala</span>
+          </div>
+          <div class="card-content-box">
+            <span class="card-content">पवित्र सलिला यमुना के तट बसा, उत्तर प्रदेश के  पश्चिमी सीमांत पर स्थित तथा हिमचाल प्रदेश की सिमा से लगता हुआ विकासनगर निरंतर विकास की और अग्रसर है । हिमालय की गोद में सुरक्षित यह नगर जनसँख्या की दृष्टि से लगातार बाद रहा
+                है तथा उसी के अनुरूप अपने आकार विस्तार में भी सतत प्रगति कर रहा है।  इस नगर के चरों और लगता हुआ विश्तृत ग्रामीण श्रेय है तथा यमुना पार जौनसार - बाबर के पर्वतीय क्षेत्र का यही एक….
+            </span>
+          </div>
+          <div class="card-button-box">
+            <span class="card-button">  <a href="goshala.php">Read More</a><span>
+          </div>
+      </div>
+
+      <div class="card">
+          <div class="card-image-box">
+            <div class="card-image" style="background-image: url(img/room.jpg);">
+            </div>
+          </div>
+
+          <div class="card-heading-box">
+            <span class="card-heading">Room </span>
+          </div>
+          <div class="card-content-box">
+            <span class="card-content">पवित्र सलिला यमुना के तट बसा, उत्तर प्रदेश के  पश्चिमी सीमांत पर स्थित तथा हिमचाल प्रदेश की सिमा से लगता हुआ विकासनगर निरंतर विकास की और अग्रसर है । हिमालय की गोद में सुरक्षित यह नगर जनसँख्या की दृष्टि से लगातार बाद रहा
+                है तथा उसी के अनुरूप अपने आकार विस्तार में भी सतत प्रगति कर रहा है।  इस नगर के चरों और लगता हुआ विश्तृत ग्रामीण श्रेय है तथा यमुना पार जौनसार - बाबर के पर्वतीय क्षेत्र का यही एक….
+            </span>
+          </div>
+          <div class="card-button-box">
+            <span class="card-button">  <a href="room.php">Read More</a><span>
+          </div>
+      </div>
+
+      <div class="card">
+          <div class="card-image-box">
+            <div class="card-image" style="background-image: url(img/goshala.jpg);">
+            </div>
+          </div>
+
+          <div class="card-heading-box">
+            <span class="card-heading">Yogshala</span>
+          </div>
+          <div class="card-content-box">
+            <span class="card-content">पवित्र सलिला यमुना के तट बसा, उत्तर प्रदेश के  पश्चिमी सीमांत पर स्थित तथा हिमचाल प्रदेश की सिमा से लगता हुआ विकासनगर निरंतर विकास की और अग्रसर है । हिमालय की गोद में सुरक्षित यह नगर जनसँख्या की दृष्टि से लगातार बाद रहा
+                है तथा उसी के अनुरूप अपने आकार विस्तार में भी सतत प्रगति कर रहा है।  इस नगर के चरों और लगता हुआ विश्तृत ग्रामीण श्रेय है तथा यमुना पार जौनसार - बाबर के पर्वतीय क्षेत्र का यही एक….
+            </span>
+          </div>
+          <div class="card-button-box">
+            <span class="card-button">  <a href="yogshala.php">Read More</a><span>
+          </div>
+      </div>
+
+    </div>
+
+    <div class="footer">
+      <div class="card-heading-box">
+        <span class="card-heading" style="color: white;">
+            hamara pata
+        </span>
+      </div>
+
+      <div class="footer-card">
+      <div class="card-content-box" style="height:auto;">
+        <span class="card-content" style="color: white; font-size:15px;"> हनुमद धाम आपकी आवश्यकताओं को पूरा करने के लिए प्रतिबद्ध है। प्रश्न, टिप्पणी या विशेष अनुरोध? हम आपसे सुनना पसंद करते हैं, इसलिए हम तक पहुंचने में संकोच न करें।
+            <br><br>पता :-   हनुमद्धाम रोड, विकासनगर देहरादून (उत्तराखंड)
+            <br>पिन कोड  :- 248198
+            <br>hanumad.dham.vn@gmail.com
+            <br>6395206598 ( विष्णु महावर )
+            <br>
+            <br><b>www.hanumaddham.org</b>
+        </span>
+      </div>
+      <hr></hr>
+      <div class="social-media">
+        <div class="social-media-link-box">
+              <span class="logo"><a href="https://www.facebook.com"><img src="img/fb.png"></a></span>
+        </div>
+        <div class="social-media-link-box">
+          <span class="logo"><a href="https://www.facebook.com"><img src="img/youtube.png"></a></span>
+        </div>
+        <div class="social-media-link-box">
+          <span class="logo"><a href="https://www.facebook.com"><img src="img/linkedin.png"></a></span>
+        </div>
+        <div class="social-media-link-box">
+          <span class="logo"><a href="https://www.facebook.com"><img src="img/instagram.png"></a></span>
+        </div>
+      </div>
+    </div>
+
+    </div>
+
+  </body>
+</html>
